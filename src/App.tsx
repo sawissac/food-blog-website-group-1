f89e7300de5e15002ce677ee3c8f5e3c0f809f07
sawissac <issacmm64@gmail.com>
2023-03-15 13:52:58 +0630

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Main from './components/Main';
 import Login from './components/Login';
 import Detail from './components/Detail';
 import Navbar from './components/Navbar';
+import Main2 from './components/Main2';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <Main2 />
       <Login />
       <Detail />
+      <Footer />
     </div>
   );
 }
