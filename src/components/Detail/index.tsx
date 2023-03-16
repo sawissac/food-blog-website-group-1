8b8385eca5301160e97b641201e1c0b37ed9c45f
unknown <thinyu466@gmail.com>
2023-03-16 10:35:35 +0630

    u

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -3,13 +3,21 @@ import styles from "./detail.module.scss";
 import { mockData } from "../../mock-data";
 
 const Detail = () => {
-  const selectedData = mockData.mockData.filter((i) => i.id === 4);
+  const selectedData = mockData.mockData.filter((i) => i.id === 2);
   const { desc, recipe, title, url } = selectedData[0];
   return (
     <section className={styles["p-detail"]}>
       <div className={styles["p-detail__image"]}>
-        <div role="img" title="blur-img" style={{ backgroundImage: `url('${url}')` }} />
-        <div role="img" title="img" style={{ backgroundImage: `url('${url}')` }} />
+        <div
+          role="img"
+          title="blur-img"
+          style={{ backgroundImage: `url('${url}')` }}
+        />
+        <div
+          role="img"
+          title="img"
+          style={{ backgroundImage: `url('${url}')` }}
+        />
       </div>
       <div className={styles["p-detail__recipes-bloc"]}>
         <div>RECIPES</div>
