5c7c6bf76f17771c80fadc5ca6aea7255b679dc4
hninpwintaung-hpa <hpwint358@gmail.com>
2023-03-20 10:04:08 +0630

    updated info-collection

--- src/context/mock-data.ts
+++ src/context/mock-data.ts
@@ -142,17 +142,12 @@ export const MockData: MockData = {
       url: "https://i.pinimg.com/736x/6c/c1/02/6cc1020d1ec013bc44fa71137d9c81bf.jpg",
     },
     {
-      title: "Baked sweet potatoes with cheese inside",
+      title: "Sliced Bread with Cinnamon Butter",
       mins: "5:00",
       id: 9,
-      recipe: "Baked Sweet",
-      desc: `Baked sweet potatoes are a popular and healthy side dish that can be enjoyed year-round. This versatile vegetable is high in fiber, vitamins, and minerals, making it an excellent addition to any diet. But have you ever considered adding cheese to your sweet potatoes? The combination of sweet and savory flavors is sure to tantalize your taste buds and leave you wanting more.
-            To prepare this delicious dish, you will need a few ingredients. You will need sweet potatoes, cheese, butter, salt, and pepper. Begin by preheating your oven to 400°F (200°C). While your oven is heating up, wash your sweet potatoes and pierce them several times with a fork. Place them on a baking sheet lined with parchment paper and bake them for 45-60 minutes or until they are tender.
-            Once your sweet potatoes are cooked, take them out of the oven and allow them to cool for a few minutes. Cut a slit in the top of each sweet potato, and carefully scoop out the flesh, leaving about 1/4 inch of the potato inside the skin. Mash the sweet potato flesh with butter, salt, and pepper to taste. Then, add your cheese of choice and mix well.
-            Spoon the mashed sweet potato and cheese mixture back into the sweet potato skins. You can sprinkle some extra cheese on top if you like. Place the stuffed sweet potatoes back onto the baking sheet and bake for an additional 10-15 minutes or until the cheese is melted and bubbly.
-            There are many different types of cheese that you can use for this recipe. Some of the most popular options include cheddar, mozzarella, and feta. Cheddar cheese is a classic choice that pairs well with the sweetness of the potato. Mozzarella cheese is another great option that will add a gooey, melty texture to the dish. Feta cheese is a tangy and salty choice that will provide a unique flavor profile.
-            Baked sweet potatoes with cheese inside are a great side dish to serve with a variety of meals. They are perfect for Thanksgiving, Christmas, or any other holiday gathering. They can also be served as a main dish for a quick and easy weeknight meal. To round out the meal, serve the sweet potatoes with a side salad or some roasted vegetables.
-            In addition to being delicious, this dish is also very nutritious. Sweet potatoes are high in fiber, vitamin A, and potassium, which are all essential nutrients for a healthy body. Cheese is a good source of protein, calcium, and vitamin D, which are all important for building and maintaining strong bones.`,
+      recipe: "Bread and Butter",
+      desc: `Sliced bread with cinnamon butter is a delicious and easy-to-make snack or breakfast option.Sliced bread with cinnamon butter is a delicious and easy-to-make snack or breakfast option. To make cinnamon butter, you can mix softened butter with ground cinnamon and a little bit of sugar to taste. Spread the cinnamon butter onto slices of your favorite bread, and then toast or bake the bread until it is golden brown and crispy. You can also add some sliced fruit or a drizzle of honey to make it even more flavorful. Enjoy!
+             To make cinnamon butter, you can mix softened butter with ground cinnamon and a little bit of sugar to taste. Spread the cinnamon butter onto slices of your favorite bread, and then toast or bake the bread until it is golden brown and crispy. You can also add some sliced fruit or a drizzle of honey to make it even more flavorful. Enjoy! `,
       url: "https://www.tasteofhome.com/wp-content/uploads/2018/01/Basic-Homemade-Bread_EXPS_TOHcom20_32480_C01_26_2b-20.jpg",
     },
     {
@@ -180,21 +175,27 @@ export const MockData: MockData = {
       title: "Banana Cream Cheesecake",
       mins: "5:00",
       id: 11,
-      recipe: "Baked Sweet",
-      desc: `Baked sweet potatoes are a popular and healthy side dish that can be enjoyed year-round. This versatile vegetable is high in fiber, vitamins, and minerals, making it an excellent addition to any diet. But have you ever considered adding cheese to your sweet potatoes? The combination of sweet and savory flavors is sure to tantalize your taste buds and leave you wanting more.
-            To prepare this delicious dish, you will need a few ingredients. You will need sweet potatoes, cheese, butter, salt, and pepper. Begin by preheating your oven to 400°F (200°C). While your oven is heating up, wash your sweet potatoes and pierce them several times with a fork. Place them on a baking sheet lined with parchment paper and bake them for 45-60 minutes or until they are tender.
-            Once your sweet potatoes are cooked, take them out of the oven and allow them to cool for a few minutes. Cut a slit in the top of each sweet potato, and carefully scoop out the flesh, leaving about 1/4 inch of the potato inside the skin. Mash the sweet potato flesh with butter, salt, and pepper to taste. Then, add your cheese of choice and mix well.
-            Spoon the mashed sweet potato and cheese mixture back into the sweet potato skins. You can sprinkle some extra cheese on top if you like. Place the stuffed sweet potatoes back onto the baking sheet and bake for an additional 10-15 minutes or until the cheese is melted and bubbly.
-            There are many different types of cheese that you can use for this recipe. Some of the most popular options include cheddar, mozzarella, and feta. Cheddar cheese is a classic choice that pairs well with the sweetness of the potato. Mozzarella cheese is another great option that will add a gooey, melty texture to the dish. Feta cheese is a tangy and salty choice that will provide a unique flavor profile.
-            Baked sweet potatoes with cheese inside are a great side dish to serve with a variety of meals. They are perfect for Thanksgiving, Christmas, or any other holiday gathering. They can also be served as a main dish for a quick and easy weeknight meal. To round out the meal, serve the sweet potatoes with a side salad or some roasted vegetables.
-            In addition to being delicious, this dish is also very nutritious. Sweet potatoes are high in fiber, vitamin A, and potassium, which are all essential nutrients for a healthy body. Cheese is a good source of protein, calcium, and vitamin D, which are all important for building and maintaining strong bones.`,
+      recipe: "Banana Cake",
+      desc: `Banana Cream Cheesecake is a delicious dessert that combines the creamy texture of cheesecake with the tropical flavor of bananas.
+            Preheat your oven to 325°F (165°C). Grease a 9-inch springform pan with cooking spray.
+            In a mixing bowl, combine graham cracker crumbs, 1/4 cup sugar, and melted butter. Mix until well combined.
+            Press the mixture into the bottom of the prepared pan and up the sides about 1 inch. Set aside.
+            In another mixing bowl, beat the cream cheese until smooth and creamy.
+            Add the 1 1/4 cups sugar and continue to beat until well combined.
+            Beat in the eggs one at a time, then add the mashed bananas and vanilla extract.
+            Mix in the flour, then pour in the heavy cream and beat until the mixture is smooth and creamy.
+            Pour the cheesecake filling into the crust-lined pan.
+            Bake in the preheated oven for 70-80 minutes or until the center is set and the edges are lightly golden.
+            Allow the cheesecake to cool for at least 1 hour at room temperature, then refrigerate for at least 4 hours or overnight.
+            To serve, remove the sides of the springform pan and garnish with whipped cream, sliced bananas, and additional graham cracker crumbs if desired.
+            Enjoy your delicious Banana Cream Cheesecake! `,
       url: "https://th.bing.com/th/id/OIP.PvEb8JGKc1RpcWIhYT9UawHaHa?pid=ImgDet&rs=1",
     },
     {
-      title: "Zesty Lime Salad Recipe",
+      title: "Zesty Lime Shrimp and Avocado Salad",
       mins: "5:00",
       id: 12,
-      recipe: "Baked Sweet",
+      recipe: "Shrimp and Avocado",
       desc: `Zesty Lime Shrimp and Avocado Salad is a refreshing and delicious dish that combines cooked shrimp, fresh avocado, crunchy vegetables, and a tangy lime dressing. Here's a basic recipe to give you an idea of how to make it:
                 •2 avocados, diced
                 •1 red onion, thinly sliced
